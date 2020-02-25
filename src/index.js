e8711ec3b730c138b4b7fb872c9abfef7057d38a
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2020-02-25 15:22:16 +0100

    [101]-setup-language-localization

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { render } from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
+import './languages/i18n';
 
 render(
   <Provider store={store}>
     <BrowserRouter>
-      <div>this is the softesue application</div>
+      <div>this is the softescu react application</div>
+      {' '}
     </BrowserRouter>
   </Provider>,
   document.getElementById('react-root'),
---
+++ src/languages/resources.js
@@ -0,0 +1,7 @@
+export const english = {
+
+};
+
+export const romania = {
+
+};
